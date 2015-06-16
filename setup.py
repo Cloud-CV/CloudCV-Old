9d97971f0036bd291dc2a50e2009ba7faf58575d
Prashant Jalan <pj.prashantjalan@gmail.com>
2015-06-16 14:51:43 +0530

    Deleting fileupload_nginx.conf link if it already exists.

--- setup.py
+++ setup.py
@@ -40,6 +40,8 @@ out = open('fileupload_nginx.conf', 'w')
 out.write(new)
 out.close()
 print "Creating a link to the /etc/nginx/sites-enabled/ folder"
+if os.path.islink('/etc/nginx/sites-enabled/fileupload_nginx.conf'):
+	os.system("rm /etc/nginx/sites-enabled/fileupload_nginx.conf")
 os.system("ln -s ./fileupload_nginx.conf /etc/nginx/sites-enabled/fileupload_nginx.conf")
 print "Done.\n"
 
