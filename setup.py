f84a99abf365201a228e214d47b1fb85e567579c
Prashant Jalan <pj.prashantjalan@gmail.com>
2015-06-17 09:15:12 +0000

    Handled file paths, now the script can run from anywhere

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ This Python script helps in the setup of the CloudCV Server code on
 a new machine. Just run this code and relax. 
 
 This code is written for Linux based systems and tested on Ubuntu 14.04
+
+Author - Prashant Jalan
 """
 
 #Module import
@@ -19,7 +21,6 @@ if not os.system('/etc/init.d/nginx status'):
 	os.system('nginx -s stop')
 
 project_path = os.path.dirname(os.path.abspath(__file__))
-print project_path
 #Pre-defined values
 values = dict()
 values['django_address']='127.0.0.1:8000'
@@ -32,36 +33,36 @@ values['virtualenv_path']='/home/ubuntu/cloudcv/ccv'
 
 #Setting up the conf.py file
 print "\nSetting up the ./app/conf.py file."
-tmp = Template(open('./app/conf.py').read())
+tmp = Template(open(project_path+'/app/conf.py').read())
 new = tmp.safe_substitute(values)
 print "Overwriting the ./app/conf.py file with system specific values."
-out = open('./app/conf.py', 'w')
+out = open(project_path+'/app/conf.py', 'w')
 out.write(new)
 out.close()
 print "Done."
 
 #Setting up the cloudcv17_uwsgi.ini file
 print "\nSetting up the cloudcv17_uwsgi.ini file."
-tmp = Template(open('./cloudcv17_uwsgi.ini').read())
+tmp = Template(open(project_path+'/cloudcv17_uwsgi.ini').read())
 new = tmp.safe_substitute(values)
 print "Overwriting the cloudcv17_uwsgi.ini file with system specific values."
-out = open('cloudcv17_uwsgi.ini', 'w')
+out = open(project_path+'/cloudcv17_uwsgi.ini', 'w')
 out.write(new)
 out.close()
 print "Done."
 
 #Setting up the fileupload_nginx.conf file
 print "\nSetting up the fileupload_nginx.conf file."
-tmp = Template(open('fileupload_nginx.conf').read())
+tmp = Template(open(project_path+'/fileupload_nginx.conf').read())
 new = tmp.safe_substitute(values)
 print "Overwriting the fileupload_nginx.conf file with system specific values."
-out = open('fileupload_nginx.conf', 'w')
+out = open(project_path+'/fileupload_nginx.conf', 'w')
 out.write(new)
 out.close()
 print "Copying the file to the /etc/nginx/sites-enabled/ folder"
 if os.path.islink('/etc/nginx/sites-enabled/fileupload_nginx.conf'):
 	os.system("rm /etc/nginx/sites-enabled/fileupload_nginx.conf")
-os.system("cp ./fileupload_nginx.conf /etc/nginx/sites-enabled/fileupload_nginx.conf")
+os.system("cp "+project_path+"/fileupload_nginx.conf /etc/nginx/sites-enabled/fileupload_nginx.conf")
 print "Done.\n"
 
 #Starting nginx server
@@ -74,7 +75,7 @@ print "\nSetting up the uwsgi process."
 if not os.path.isdir('/etc/uwsgi'):
 	os.system('mkdir /etc/uwsgi')
 print "Copying the cloudcv17_uwsgi.ini to /etc/uwsgi"
-os.system('cp cloudcv17_uwsgi.ini /etc/uwsgi/')
+os.system('cp '+project_path+'/cloudcv17_uwsgi.ini /etc/uwsgi/')
 print "Creating upstart for uWSGI emperor"
 script = """
 # Emperor uWSGI script
