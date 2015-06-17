f334354f7b5b9da056c6d7235d312623e2b5f10e
DESHRAJ <deshrajdry@gmail.com>
2015-06-17 22:38:40 +0000

    serializers updated according to new models

--- app/serializers.py
+++ app/serializers.py
@@ -32,10 +32,10 @@ class RequestLogSerializer(serializers.ModelSerializer):
 	'''
 	Serializer Class for RequestLog Model
 	'''
-	user_entries = UserSerializer(many=True)
+	# user_fields = UserSerializer(source = "user")
 	class Meta:
 		model = RequestLog
-		fields = ('api_used','job_id','processing_state','no_of_images','parameters','duration','input_source_type','input_source_value','output_source_type','output_source_value')
+		fields = ('user','api_used','job_id','processing_state','no_of_images','parameters','duration','input_source_type','input_source_value','output_source_type','output_source_value')
 
 	def create(self, validated_data):
 		"""
@@ -54,35 +54,15 @@ class RequestLogSerializer(serializers.ModelSerializer):
 		return instance
 
 
-class GroupSerializer(serializers.ModelSerializer):
-	'''
-	Serializer Class for Group Model
-	'''
-	user_entries = UserSerializer(many=True)
-	class Meta:
-		fields = ('group_id','group_name','purpose','user')
-
-	def create(self, validated_data):
-		"""
-		Create and return a new `Group` instance, given the validated data.
-		"""
-		return Group.objects.create(**validated_data)
-
-	def update(seld, instance, validated_data):
-		instance.group_id = validated_data.get('group_id',instance.group_id)
-		instance.group_name = validated_data.get('group_name',instance.group_name)
-		instance.purpose = validated_data.get('purpose',instance.purpose)
-		# instance.user = validated_data.get('user',instance.user)
-		instance.save()
-		return instance
 
 class CurrentRequestSerializer(serializers.ModelSerializer):
 	'''
 	Serializer Class for CurrentRequest Model
 	'''
-	user_entries = UserSerializer(many=True)
+	# user_fields = UserSerializer(source = "user")
 	class Meta:
-		fields = ('ram_usage','cpu_usage','disk_space_usage','no_of_jobs_running')
+		model = CurrentRequest
+		fields = ('user','ram_usage','cpu_usage','disk_space_usage','no_of_jobs_running')
 
 	def create(self, validated_data):
 		"""
@@ -90,7 +70,7 @@ class CurrentRequestSerializer(serializers.ModelSerializer):
 		"""
 		return CurrentRequest.objects.create(**validated_data)
 
-	def update(seld, instance, validated_data):
+	def update(self, instance, validated_data):
 		instance.ram_usage = validated_data.get('ram_usage',instance.ram_usage)
 		instance.cpu_usage = validated_data.get('cpu_usage',instance.cpu_usage)
 		instance.disk_space_usage = validated_data.get('disk_space_usage',instance.disk_space_usage)
@@ -103,9 +83,10 @@ class ImagesSerializer(serializers.ModelSerializer):
 	'''
 	Serializer Class for Images Model
 	'''
-	user_entries = UserSerializer(many=True)
+	# user_fields = UserSerializer(source = "user")
 	class Meta:
-		fields = ('category','url')
+		model = Images
+		fields = ('user','category','url')
 
 	def create(self,validated_data):
 		"""
@@ -113,9 +94,54 @@ class ImagesSerializer(serializers.ModelSerializer):
 		"""
 		return Images.objects.create(**validated_data)
 
-	def update(seld, instance, validated_data):
+	def update(self, instance, validated_data):
 		instance.category = validated_data.get('category',instance.category)
 		instance.url = validated_data.get('url',instance.url)
+		# instance.user = validated_data.get('user',instance.user)
+		instance.save()
+		return instance
+
+class ModelStorageSerializer(serializers.ModelSerializer):
+	'''
+	Serializer Class for ModelStorage Model
+	'''
+	class Meta:
+		model = ModelStorage
+		fields = ('file_location','parameters', 'neural_network','database_used')
+
+	def create(self,validated_data):
+		"""
+		Create and return a new `ModelStorage` instance, given the validated data.
+		"""
+		return ModelStorage.objects.create(**validated_data)
+
+	def update(self, instance, validated_data):
+		instance.file_location = validated_data.get('file_location',instance.file_location)
+		instance.parameters = validated_data.get('parameters',instance.parameters)
+		instance.neural_network = validated_data.get('neural_network',instance.neural_network)
+		instance.database_used = validated_data.get('database_used',instance.database_used)
+		
+		instance.save()
+		return instance
+		
+class GroupSerializer(serializers.ModelSerializer):
+	'''
+	Serializer Class for Group Model
+	'''
+	class Meta:
+		model = Group
+		fields = ('model','group_id','group_name','purpose','user')
+
+	def create(self, validated_data):
+		"""
+		Create and return a new `Group` instance, given the validated data.
+		"""
+		return Group.objects.create(**validated_data)
+
+	def update(self, instance, validated_data):
+		instance.group_id = validated_data.get('group_id',instance.group_id)
+		instance.group_name = validated_data.get('group_name',instance.group_name)
+		instance.purpose = validated_data.get('purpose',instance.purpose)
 		# instance.user = validated_data.get('user',instance.user)
 		instance.save()
 		return instance
