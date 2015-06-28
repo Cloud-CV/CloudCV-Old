b1040f3b4ec5baf91ad3a2fb87edf60f85da27b5
Prashant Jalan <pj.prashantjalan@gmail.com>
2015-06-28 16:11:08 +0000

    Included Nginx

--- Docker/server.sh
+++ Docker/server.sh
@@ -12,3 +12,9 @@ cp ../nodejs/chat.js ./Node/
 sudo docker build -t cloudcv/node ./Node/
 sudo docker run -d --link cloudcv_redis:redis --name cloudcv_node cloudcv/node
 rm ./Node/chat.js
+
+echo "Pulling the image and starting nginx server"
+cp ../fileupload_nginx.conf ./Nginx/default.conf
+sudo docker build -t cloudcv/nginx ./Nginx/
+sudo docker run -d -p 80:80 -p 443:443 --name cloudcv_nginx --link cloudcv_node:node --link cloudcv_redis:redis cloudcv/nginx
+rm Nginx/default.conf
