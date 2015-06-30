a3f1ad37175ee4a18b581c743a102a463faa5f57
Deshraj <deshrajdry@gmail.com>
2015-07-01 03:15:48 +0530

    urls changed according to social login and cloud storage

--- allauth/socialaccount/urls.py
+++ allauth/socialaccount/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     url('^login/cancelled/$', views.login_cancelled, 
         name='socialaccount_login_cancelled'),
     url('^login/error/$', views.login_error, name='socialaccount_login_error'),
-    url('^signup/$', views.signup, name='socialaccount_signup'),
-    url('^connections/$', views.connections, name='socialaccount_connections'))
+    url('^social_signup/$', views.signup, name='socialaccount_signup'),
+    url('^cloudstorage/$', views.connections, name='socialaccount_connections'))
