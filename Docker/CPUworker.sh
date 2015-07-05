a2cfb0aa8cf1c9aee09198bb417a3d1a1872341a
Prashant Jalan <pj.prashantjalan@gmail.com>
2015-07-05 22:30:40 +0530

    Corrected name

--- Docker/CPUworker.sh
+++ Docker/CPUworker.sh
@@ -5,6 +5,6 @@
 
 echo "Pulling the image and starting worker server for CPU"
 wget -P ./Celery/ http://dl.caffe.berkeleyvision.org/bvlc_reference_caffenet.caffemodel
-sudo docker build -t cloudcv/CPUworker ./CPUWorker/
-sudo docker run -d --volumes-from cloudcv_code --link cloudcv_redis:redis --link rabbitmq:rabbitmq --name cloudcv_CPUworker cloudcv/CPUworker
+sudo docker build -t cloudcv/cpu-worker ./CPUWorker/
+sudo docker run -d --volumes-from cloudcv_code --link cloudcv_redis:redis --link rabbitmq:rabbitmq --name cloudcv_cpu_worker cloudcv/cpu-worker
 rm ./Celery/bvlc_reference_caffenet.caffemodel
