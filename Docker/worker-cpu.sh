b3b06c08b8bd4e19f6fadbeea329d38d09cade95
Prashant Jalan <pj.prashantjalan@gmail.com>
2015-07-07 22:40:57 +0000

    Corrected folder path

--- Docker/worker-cpu.sh
+++ Docker/worker-cpu.sh
@@ -4,7 +4,7 @@
 # Author: Prashant Jalan
 
 echo "Pulling the image and starting worker server for CPU"
-wget -P ./Celery/ http://dl.caffe.berkeleyvision.org/bvlc_reference_caffenet.caffemodel
+wget -P ./CPUWorker http://dl.caffe.berkeleyvision.org/bvlc_reference_caffenet.caffemodel
 sudo docker build -t cloudcv/cpu-worker ./CPUWorker/
 sudo docker run -d --volumes-from cloudcv_code --link cloudcv_rabbitmq:rabbitmq --link cloudcv_redis:redis --name cloudcv_worker_cpu cloudcv/cpu-worker
-rm ./Celery/bvlc_reference_caffenet.caffemodel
+rm ./CPUWorker/bvlc_reference_caffenet.caffemodel
