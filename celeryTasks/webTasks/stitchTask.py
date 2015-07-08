b357459d8568bfe1d92b7aba566be03126f31b1a
Prashant Jalan <pj.prashantjalan@gmail.com>
2015-07-08 15:03:58 +0530

    Taking arg of src, output and result

--- celeryTasks/webTasks/stitchTask.py
+++ celeryTasks/webTasks/stitchTask.py
@@ -4,6 +4,7 @@ from celeryTasks.celery import app
 # The function takes as input:
 # 1) src_path: Input directory.
 # 2) socketid: The socket id of the connection.
+# 3) output_path: The directory where to store the result stitch image.
 # 4) result_path: Directory accessible by web.
 #    It should be full path in case to the result_stitch image.
 # NOTE:
@@ -11,7 +12,7 @@ from celeryTasks.celery import app
 # 2) ignore_result=True signifies that celery won't pass any result to the backend.
 # 3) It is important to import all the modules only inside the function.
 @app.task(ignore_result=True)
-def stitchImages(src_path, socketid, result_path):
+def stitchImages(src_path, socketid, output_path, result_path):
     
     #Establishing connection to send results and write messages
     import redis, json
@@ -22,6 +23,7 @@ def stitchImages(src_path, socketid, result_path):
 
         #Execute the stitch_full executable
         rs.publish('chat', json.dumps({'message': src_path, 'socketid': str(socketid)}))
+        rs.publish('chat', json.dumps({'message': output_path, 'socketid': str(socketid)}))
         rs.publish('chat', json.dumps({'message': result_path, 'socketid': str(socketid)}))
 
 
