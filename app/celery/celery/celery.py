2446b8074964bb6cde2f8766f0fa6a5f52394ce7
Prashant Jalan <pj.prashantjalan@gmail.com>
2015-07-08 20:27:07 +0530

    Deprecated. Moved to new worker

--- app/celery/celery/celery.py
+++ app/celery/celery/celery.py
@@ -1,14 +1,14 @@
-from __future__ import absolute_import
+# from __future__ import absolute_import
 
-from celery import Celery
+# from celery import Celery
 
-# instantiate Celery object
-celery = Celery(include=[
-                         'app.celery.api_tasks.tasks'
-                        ])
+# # instantiate Celery object
+# celery = Celery(include=[
+#                          'app.celery.api_tasks.tasks'
+#                         ])
 
-# import celery config file
-celery.config_from_object('app.celery.celeryconfig')
+# # import celery config file
+# celery.config_from_object('app.celery.celeryconfig')
 
-if __name__ == '__main__':
-    celery.start()
+# if __name__ == '__main__':
+#     celery.start()
