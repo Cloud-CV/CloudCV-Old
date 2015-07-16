9fd98e9773965954ea026ce93a4280c3fd53ed00
Deshraj <deshrajdry@gmail.com>
2015-07-16 13:45:27 +0530

    Picture Table added again in database

--- app/models.py
+++ app/models.py
@@ -126,3 +126,29 @@ class StorageCredentials(models.Model):
     user  = models.ForeignKey(User)
     aws_access_key = models.CharField(max_length = 100)
     aws_access_secret = models.CharField(max_length = 100)
+
+class Picture(models.Model):
+    """This is a small demo using just two fields. The slug field is really not
+    necessary, but makes the code simpler. ImageField depends on PIL or
+    pillow (where Pillow is easily installable in a virtualenv. If you have
+    problems installing pillow, use a more generic FileField instead.
+    """
+    file = models.ImageField(upload_to="pictures")
+    slug = models.SlugField(max_length=50, blank=True)
+
+    def __unicode__(self):
+        return self.file.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('upload-new', )
+
+    def save(self, *args, **kwargs):
+        self.slug = self.file.name
+        super(Picture, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        """delete -- Remove to leave file."""
+        self.file.delete(False)
+        super(Picture, self).delete(*args, **kwargs)
+
