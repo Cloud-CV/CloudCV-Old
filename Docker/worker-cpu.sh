9d01e513b3ffe2d3e086ebb81faf410a7045b188
DESHRAJ <deshrajdry@gmail.com>
2015-07-19 11:58:43 +0000

    DIGITS aded in Docker

--- Docker/worker-cpu.sh
+++ Docker/worker-cpu.sh
@@ -5,6 +5,6 @@
 
 echo "Pulling the image and starting worker server for CPU"
 wget -P ./CPUWorker http://dl.caffe.berkeleyvision.org/bvlc_reference_caffenet.caffemodel
-sudo docker build -t cloudcv/cpu-worker ./CPUWorker/
-sudo docker run -d --volumes-from cloudcv_code --link cloudcv_rabbitmq:rabbitmq --link cloudcv_redis:redis --name cloudcv_worker_cpu cloudcv/cpu-worker
+sudo docker build -t cloudcv/cpu-desh-worker ./CPUWorker/
+sudo docker run -d --volumes-from cloudcv_desh_code --link cloudcv_desh_rabbitmq:rabbitmq --link cloudcv_desh_redis:redis --name cloudcv_desh_worker_cpu cloudcv/cpu-desh-worker
 rm ./CPUWorker/bvlc_reference_caffenet.caffemodel
