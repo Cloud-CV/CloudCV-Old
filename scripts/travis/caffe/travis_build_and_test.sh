cfa8c082f3ba6eb96431db2aa4c17ed01d00bd53
deshraj <deshrajdry@gmail.com>
2016-03-13 02:03:29 +0530

    minor bug fix

--- scripts/travis/caffe/travis_build_and_test.sh
+++ scripts/travis/caffe/travis_build_and_test.sh
@@ -3,6 +3,10 @@
 # Travis CI tests are CPU-only for lack of compatible hardware.
 
 set -e
+# change permissions for installed python packages
+sudo chown $USER -R ~/miniconda
+sudo chown $USER -R ~/.cache
 
+cd $(pwd)/deps/caffe
 make --jobs=$NUM_THREADS all
 make --jobs=$NUM_THREADS pycaffe
