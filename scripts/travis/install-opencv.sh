8a4aee0bbfceafaaa3feb1775e8d86affcf9e568
deshraj <deshrajdry@gmail.com>
2016-03-12 15:53:12 +0530

    testing docker containers in travis

--- scripts/travis/install-opencv.sh
+++ scripts/travis/install-opencv.sh
@@ -7,21 +7,21 @@ then
     exit 1
 fi
 
-INSTALL_DIR=$1
+INSTALL_DIR=$(pwd)/dep
 mkdir -p $INSTALL_DIR
 
 # Install dependencies
 sudo apt-get install -y gfortran git wget unzip build-essential
 
 # Download source code
-wget -O $INSTALL_DIR/OpenCV-2.4.11.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.11/opencv-2.4.11.zip/download && unzip $INSTALL_DIR/OpenCV-2.4.11.zip && mv $INSTALL_DIR/opencv-2.4.11 $INSTALL_DIR && rm $INSTALL_DIR/OpenCV-2.4.11.zip
+wget -O $INSTALL_DIR/OpenCV-2.4.11.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.11/opencv-2.4.11.zip/download && unzip $INSTALL_DIR/OpenCV-2.4.11.zip && mv $INSTALL_DIR/opencv-2.4.11 $INSTALL_DIR/opencv && rm $INSTALL_DIR/OpenCV-2.4.11.zip
 
 # OpenCV Installation
 # OpenCV dependencies
 sudo apt-get install -y cmake libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install -y python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 
-cd $INSTALL_DIR && \
+cd $INSTALL_DIR/opencv && \
     mkdir release && \
     cd release && \
     cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local .. && \
