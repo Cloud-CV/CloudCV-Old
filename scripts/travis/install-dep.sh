bada761ed89fcade18a3a9b491ce5fd1ab771146
deshraj <deshrajdry@gmail.com>
2016-03-12 18:54:39 +0530

    minor directory changes

--- scripts/travis/install-dep.sh
+++ scripts/travis/install-dep.sh
@@ -4,15 +4,16 @@ mkdir -p $(pwd)/dep/caffe
 mkdir -p $(pwd)/dep/opencv
 # Download source code
 # git clone https://github.com/graphlab-code/graphlab.git
-wget -O $(pwd)/dep/rc2.zip https://github.com/BVLC/caffe/archive/rc2.zip && unzip $(pwd)/dep/rc2 && mv $(pwd)/dep/caffe-rc2 $(pwd)/dep/caffe && rm $(pwd)/dep/rc2.zip
-wget -O $(pwd)/dep/OpenCV-2.4.11.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.11/opencv-2.4.11.zip/download && unzip $(pwd)/dep/OpenCV-2.4.11.zip && mv $(pwd)/dep/opencv-2.4.11 $(pwd)/dep/opencv && rm $(pwd)/dep/OpenCV-2.4.11.zip
+cd $(pwd)/dep
+wget -O rc2.zip https://github.com/BVLC/caffe/archive/rc2.zip && unzip rc2 && mv caffe-rc2 caffe && rm rc2.zip
+wget -O OpenCV-2.4.11.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.11/opencv-2.4.11.zip/download && unzip OpenCV-2.4.11.zip && mv opencv-2.4.11 opencv && rm OpenCV-2.4.11.zip
 
 # OpenCV Installation
 # OpenCV dependencies
 sudo apt-get install -y cmake libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install -y python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 
-cd $(pwd)/dep/opencv && \
+cd opencv && \
     mkdir release && \
     cd release && \
     cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local .. && \
@@ -34,7 +35,7 @@ cd $(pwd)/dep/caffe && \
     mkdir build && \
     cd build && \
     cmake .. && \
-    make -j 4 all 
+    make -j 4 all
 
 # In order to import caffe in python
 export PYTHONPATH=$PYTHONPATH:$(pwd)/dep/caffe/python
