0fc75e9631010d3cef67106dbd6027560a450034
deshraj <deshrajdry@gmail.com>
2016-03-12 14:34:35 +0530

    dependenices differentiated into different scripts

---
+++ scripts/travis/install-caffe.sh
@@ -0,0 +1,41 @@
+set -e
+set -x
+
+if [ "$#" -ne 1 ];
+then
+    echo "Usage: $0 INSTALL_DIR"
+    exit 1
+fi
+
+INSTALL_DIR=$1
+mkdir -p $INSTALL_DIR
+
+# Download source code
+wget -O $INSTALL_DIR/caffe.zip https://github.com/BVLC/caffe/archive/rc2.zip && unzip $INSTALL_DIR/caffe
+
+# Caffe installation
+# Caffe dependencies
+sudo apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev
+sudo apt-get install -y --no-install-recommends libboost-all-dev
+sudo apt-get install -y libatlas-base-dev
+sudo apt-get install -y libgflags-dev libgoogle-glog-dev liblmdb-dev protobuf-compiler
+
+for req in $(cat $INSTALL_DIR/caffe/python/requirements.txt)
+do
+    pip install $req
+done
+
+cd $INSTALL_DIR/caffe && \
+    mkdir build && \
+    cd build && \
+    cmake .. && \
+    make -j 4 all 
+
+# In order to import caffe in python
+export PYTHONPATH=$PYTHONPATH:$INSTALL_DIR/caffe/python
+
+# Copying the required caffe model
+cp $INSTALL_DIR/caffe/build/bvlc_reference_caffenet.caffemodel $INSTALL_DIR/caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel
+
+export C_FORCE_ROOT=TRUE
+# CMD ["celery","-A","celeryTasks","worker","--loglevel=debug", "--logfile=/CloudCV_Server/celery.log"]
---
+++ scripts/travis/install-opencv.sh
@@ -0,0 +1,29 @@
+set -e
+set -x
+
+if [ "$#" -ne 1 ];
+then
+    echo "Usage: $0 INSTALL_DIR"
+    exit 1
+fi
+
+INSTALL_DIR=$1
+mkdir -p $INSTALL_DIR
+
+# Install dependencies
+sudo apt-get install -y gfortran git wget unzip build-essential
+
+# Download source code
+wget -O $INSTALL_DIR/opencv.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.11/opencv-2.4.11.zip/download && unzip $INSTALL_DIR/opencv.zip
+
+# OpenCV Installation
+# OpenCV dependencies
+sudo apt-get install -y cmake libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+
+cd $INSTALL_DIR/opencv && \
+    mkdir release && \
+    cd release && \
+    cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local .. && \
+    make -j 4 && \
+    make install
