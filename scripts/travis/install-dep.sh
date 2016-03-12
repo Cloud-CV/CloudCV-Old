2c801a10a3c797aafa213c08d2252e32f872a0d9
deshraj <deshrajdry@gmail.com>
2016-03-13 00:39:56 +0530

    caffe build separated from all other dependencies

--- scripts/travis/install-dep.sh
+++ scripts/travis/install-dep.sh
@@ -3,11 +3,7 @@ mkdir -p $INSTALL_DIR/caffe
 mkdir -p $INSTALL_DIR/opencv
 
 # Install dependencies
-sudo -E ./scripts/travis/travis_install.sh
 cd $INSTALL_DIR
-# change permissions for installed python packages
-sudo chown $USER -R ~/miniconda
-sudo chown $USER -R ~/.cache
 
 # Download Opencv source code
 wget -O OpenCV-2.4.11.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.11/opencv-2.4.11.zip/download && unzip -qq OpenCV-2.4.11.zip && mv opencv-2.4.11 opencv && rm OpenCV-2.4.11.zip
@@ -20,36 +16,6 @@ cd $INSTALL_DIR/opencv && \
     make -j 4 && \
     make install
 
-# Caffe installation
-wget https://github.com/BVLC/caffe/archive/rc2.zip && unzip -qq rc2 && mv caffe-rc2/* caffe && rm rc2.zip
-cd $INSTALL_DIR/caffe
-
-# Build source
-cp Makefile.config.example Makefile.config
-sed -i 's/# CPU_ONLY/CPU_ONLY/g' Makefile.config
-sed -i 's/USE_CUDNN/#USE_CUDNN/g' Makefile.config
-
-# Use miniconda
-sed -i 's/# ANACONDA_HOME/ANACONDA_HOME/' Makefile.config
-sed -i 's/# PYTHON_INCLUDE/PYTHON_INCLUDE/' Makefile.config
-sed -i 's/# $(ANACONDA_HOME)/$(ANACONDA_HOME)/' Makefile.config
-sed -i 's/# PYTHON_LIB/PYTHON_LIB/' Makefile.config
-sed -i 's/ANACONDA/MINICONDA/g' Makefile.config
-sed -i 's/Anaconda/Miniconda/g' Makefile.config
-sed -i 's/anaconda/miniconda/g' Makefile.config
-echo 'LINKFLAGS += -Wl,-rpath,/home/travis/miniconda/lib' >> Makefile.config
-
-# compile
-mkdir build && cd build && cmake .. && make -j 4 all
-
-# Install python dependencies
-# conda (fast)
-conda install --yes cython nose ipython h5py pandas python-gflags
-
-for req in $(cat $INSTALL_DIR/caffe/python/requirements.txt)
-do
-    pip install $req
-done
 # In order to import caffe in python
 export PYTHONPATH=$PYTHONPATH:$INSTALL_DIR/caffe/python
 
--- scripts/travis/install-opencv.sh
+++ scripts/travis/install-opencv.sh
@@ -1,29 +1,38 @@
+#!/bin/bash
+# This script must be run with sudo.
+
 set -e
-set -x
-
-if [ "$#" -ne 1 ];
-then
-    echo "Usage: $0 INSTALL_DIR"
-    exit 1
-fi
-
-INSTALL_DIR=$1
-mkdir -p $INSTALL_DIR
 
 # Install dependencies
-sudo apt-get install -y gfortran git wget unzip build-essential
+apt-get install -y gfortran git wget unzip build-essential
 
-# Download source code
-wget -O $INSTALL_DIR/OpenCV-2.4.11.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.11/opencv-2.4.11.zip/download && unzip $INSTALL_DIR/OpenCV-2.4.11.zip && mv $INSTALL_DIR/opencv-2.4.11 $INSTALL_DIR && rm $INSTALL_DIR/OpenCV-2.4.11.zip
-# OpenCV Installation
 # OpenCV dependencies
-sudo apt-get install -y cmake libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install -y python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+apt-get install -y cmake libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+apt-get install -y python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 
+INSTALL_DIR=$(pwd)/dep
+mkdir -p $INSTALL_DIR/opencv
 
-cd $INSTALL_DIR && \
+# Install dependencies
+cd $INSTALL_DIR
+
+# Download Opencv source code
+wget -O OpenCV-2.4.11.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.11/opencv-2.4.11.zip/download && unzip -qq OpenCV-2.4.11.zip && mv opencv-2.4.11 opencv && rm OpenCV-2.4.11.zip
+
+# OpenCV Installation
+cd $INSTALL_DIR/opencv && \
     mkdir release && \
     cd release && \
     cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local .. && \
     make -j 4 && \
     make install
+
+# In order to import caffe in python
+export PYTHONPATH=$PYTHONPATH:$INSTALL_DIR/caffe/python
+
+# Copying the required caffe model
+mkdir -p $INSTALL_DIR/caffe/models/bvlc_reference_caffenet
+wget -nc -P $INSTALL_DIR/caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel http://dl.caffe.berkeleyvision.org/bvlc_reference_caffenet.caffemodel
+
+export C_FORCE_ROOT=TRUE
+# CMD ["celery","-A","celeryTasks","worker","--loglevel=debug", "--logfile=/CloudCV_Server/celery.log"]
