63c6e127bcbb1407e3b34ba5e1932bf7db2ca1f9
deshraj <deshrajdry@gmail.com>
2016-03-12 22:14:58 +0530

    anaconda added

--- scripts/travis/install-dep.sh
+++ scripts/travis/install-dep.sh
@@ -1,19 +1,18 @@
-# Install dependencies
-sudo apt-get install -y gfortran git wget unzip build-essential
-
 INSTALL_DIR=$(pwd)/dep
 mkdir -p $INSTALL_DIR/caffe
 mkdir -p $INSTALL_DIR/opencv
 
+# Install dependencies
+sudo -E ./scripts/travis/travis_install.sh
 cd $INSTALL_DIR
-# Download source code
+# change permissions for installed python packages
+sudo chown $USER -R ~/miniconda
+sudo chown $USER -R ~/.cache
+
+# Download Opencv source code
 wget -O OpenCV-2.4.11.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.11/opencv-2.4.11.zip/download && unzip -qq OpenCV-2.4.11.zip && mv opencv-2.4.11 opencv && rm OpenCV-2.4.11.zip
 
 # OpenCV Installation
-# OpenCV dependencies
-sudo apt-get install -y cmake libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install -y python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
-
 cd $INSTALL_DIR/opencv && \
     mkdir release && \
     cd release && \
@@ -21,28 +20,36 @@ cd $INSTALL_DIR/opencv && \
     make -j 4 && \
     make install
 
-
+# Caffe installation
 wget https://github.com/BVLC/caffe/archive/rc2.zip && unzip -qq rc2 && mv caffe-rc2/* caffe && rm rc2.zip
+cd $INSTALL_DIR/caffe
 
 # Build source
+cp Makefile.config.example Makefile.config
+sed -i 's/# CPU_ONLY/CPU_ONLY/g' Makefile.config
+sed -i 's/USE_CUDNN/#USE_CUDNN/g' Makefile.config
 
-# Caffe installation
-# Caffe dependencies
-sudo apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev
-sudo apt-get install -y --no-install-recommends libboost-all-dev
-sudo apt-get install -y libatlas-base-dev
-sudo apt-get install -y libgflags-dev libgoogle-glog-dev liblmdb-dev protobuf-compiler
+# Use miniconda
+sed -i 's/# ANACONDA_HOME/ANACONDA_HOME/' Makefile.config
+sed -i 's/# PYTHON_INCLUDE/PYTHON_INCLUDE/' Makefile.config
+sed -i 's/# $(ANACONDA_HOME)/$(ANACONDA_HOME)/' Makefile.config
+sed -i 's/# PYTHON_LIB/PYTHON_LIB/' Makefile.config
+sed -i 's/ANACONDA/MINICONDA/g' Makefile.config
+sed -i 's/Anaconda/Miniconda/g' Makefile.config
+sed -i 's/anaconda/miniconda/g' Makefile.config
+echo 'LINKFLAGS += -Wl,-rpath,/home/travis/miniconda/lib' >> Makefile.config
+
+# compile
+mkdir build && cd build && cmake .. && make -j 4 all
+
+# Install python dependencies
+# conda (fast)
+conda install --yes cython nose ipython h5py pandas python-gflags
+
 for req in $(cat $INSTALL_DIR/caffe/python/requirements.txt)
 do
     pip install $req
 done
-
-cd $INSTALL_DIR/caffe
-cp Makefile.config.example Makefile.config
-sed -i 's/# CPU_ONLY/CPU_ONLY/g' Makefile.config
-sed -i 's/USE_CUDNN/#USE_CUDNN/g' Makefile.config
-mkdir build && cd build && cmake .. && make -j 4 all
-
 # In order to import caffe in python
 export PYTHONPATH=$PYTHONPATH:$INSTALL_DIR/caffe/python
 
