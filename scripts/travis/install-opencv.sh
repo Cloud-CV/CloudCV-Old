b707320e9c731db88897eb08e01e2c96f4c48a2b
deshraj <deshrajdry@gmail.com>
2016-03-15 01:48:40 +0530

    opencv reconfigured

--- scripts/travis/install-opencv.sh
+++ scripts/travis/install-opencv.sh
@@ -4,45 +4,20 @@
 set -e
 
 # Install dependencies
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install build-essential cmake git pkg-config
-sudo apt-get install libjpeg8-dev libtiff4-dev libjasper-dev libpng12-dev
-sudo apt-get install libgtk2.0-dev
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt-get install libatlas-base-dev gfortran
-sudo apt-get install python2.7-dev
+apt-get install -y gfortran git wget unzip build-essential
 
-# clone source
+# OpenCV dependencies
+apt-get install -y cmake libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+apt-get install -y python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+
+# Download Opencv source code
 cd ~
-git clone https://github.com/Itseez/opencv.git
-cd opencv
-git checkout 3.0.0
+wget -O OpenCV-2.4.11.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.11/opencv-2.4.11.zip/download && unzip -qq OpenCV-2.4.11.zip && mv opencv-2.4.11 opencv && rm OpenCV-2.4.11.zip
 
-# SIFT SURF are in opencv_contrib.git
-cd ~
-git clone https://github.com/Itseez/opencv_contrib.git
-cd opencv_contrib
-git checkout 3.0.0
-
-cd ~/opencv
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=RELEASE \
- -DOPENCV_EXTRA_MODULES_PATH=~/github/opencv_contrib/modules \
- -DWITH_TBB=ON \
- -DBUILD_NEW_PYTHON_SUPPORT=ON \
- -DINSTALL_C_EXAMPLES=ON \
- -DINSTALL_PYTHON_EXAMPLES=ON \
- -DBUILD_EXAMPLES=ON \
- -DWITH_CUDA=OFF \
- -DBUILD_TIFF=ON \
- -DCMAKE_INSTALL_PREFIX=$(python -c "import sys; print(sys.prefix)") \
- -DPYTHON_EXECUTABLE=$(which python) \
- -DPYTHON_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
- -DPYTHON_PACKAGES_PATH=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
- -DCMAKE_INSTALL_PREFIX=/usr/local \ #specify any location you want to install
-
-make -j4
-sudo make install
-sudo ldconfig
+# OpenCV Installation
+cd opencv && \
+    mkdir release && \
+    cd release && \
+    cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local .. && \
+    make -j 4 && \
+    make install
