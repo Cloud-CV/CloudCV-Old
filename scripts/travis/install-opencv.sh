2163faca99f5a3767c62672af28cdcd53ca59ed0
deshraj <deshrajdry@gmail.com>
2016-03-15 03:10:34 +0530

    Debug statements added for testing

--- scripts/travis/install-opencv.sh
+++ scripts/travis/install-opencv.sh
@@ -3,8 +3,13 @@
 
 set -e
 
+echo "############################################"
+echo $HOME
+echo "############################################"
+
 # check to see if protobuf folder is empty
-if [ ! -d "$HOME/opencv/release" ]; then
+cd ~
+if [ ! -d "opencv/release" ]; then
   # Install dependencies
   apt-get install -y gfortran git wget unzip build-essential
 
@@ -13,7 +18,6 @@ if [ ! -d "$HOME/opencv/release" ]; then
   apt-get install -y python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 
   # Download Opencv source code
-  cd ~
   wget -O OpenCV-2.4.11.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.11/opencv-2.4.11.zip/download && unzip -qq OpenCV-2.4.11.zip && mv opencv-2.4.11 opencv && rm OpenCV-2.4.11.zip
 
   # OpenCV Installation
