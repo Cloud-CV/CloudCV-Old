0ee6e7c67aa6c1dbd7afe54e58922c0b898a97c5
deshraj <deshrajdry@gmail.com>
2016-03-15 20:22:59 +0530

    Addition of other debug statements to check the path of installation

--- scripts/travis/install-opencv.sh
+++ scripts/travis/install-opencv.sh
@@ -3,13 +3,8 @@
 
 set -e
 
-echo "############################################"
-echo $HOME
-echo "############################################"
-
 # check to see if protobuf folder is empty
-cd ~
-if [ ! -d "opencv/release" ]; then
+if [ ! -d "$(HOME)/opencv/release" ]; then
   # Install dependencies
   apt-get install -y gfortran git wget unzip build-essential
 
@@ -18,22 +13,17 @@ if [ ! -d "opencv/release" ]; then
   apt-get install -y python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 
   # Download Opencv source code
+  cd ~
   wget -O OpenCV-2.4.11.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.11/opencv-2.4.11.zip/download && unzip -qq OpenCV-2.4.11.zip && mv opencv-2.4.11 opencv && rm OpenCV-2.4.11.zip
 
   # OpenCV Installation
-  echo "Error Catch Point: 1"
   cd opencv
-  echo "Error Catch Point: 2"
   mkdir release
-  echo "Error Catch Point: 3"
   cd release
-  echo "Error Catch Point: 4"
+  echo $(pwd)
   cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
-  echo "Error Catch Point: 5"
   make -j 4
-  echo "Error Catch Point: 6"
   make install
-  echo "Error Catch Point: 7"
 else
   echo 'Using cached directory for OpenCV Installation.';
 fi
