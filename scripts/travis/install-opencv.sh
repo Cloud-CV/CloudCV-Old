e01c792fec5b7f8e7c2b5f9e4a062f2cdf86040b
deshraj <deshrajdry@gmail.com>
2016-03-15 21:08:47 +0530

    path changed for opencv installation

--- scripts/travis/install-opencv.sh
+++ scripts/travis/install-opencv.sh
@@ -4,7 +4,8 @@
 set -e
 
 # check to see if protobuf folder is empty
-if [ ! -d "$(HOME)/opencv/release" ]; then
+
+if [ ! -d "$(pwd)/opencv/release" ]; then
   # Install dependencies
   apt-get install -y gfortran git wget unzip build-essential
 
@@ -13,14 +14,12 @@ if [ ! -d "$(HOME)/opencv/release" ]; then
   apt-get install -y python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 
   # Download Opencv source code
-  cd ~
   wget -O OpenCV-2.4.11.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.11/opencv-2.4.11.zip/download && unzip -qq OpenCV-2.4.11.zip && mv opencv-2.4.11 opencv && rm OpenCV-2.4.11.zip
 
   # OpenCV Installation
   cd opencv
   mkdir release
   cd release
-  echo $(pwd)
   cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
   make -j 4
   make install
